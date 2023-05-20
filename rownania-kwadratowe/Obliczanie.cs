b55af4a68e2eb8711134907748e6886c8d5022c0
K4animation <k4animation@gmail.com>
2023-05-21 01:07:32 +0200

    Poprawka w metodzie obliczania, aby testy jednostkowe dzialaly lepiej

--- rownania-kwadratowe/Obliczanie.cs
+++ rownania-kwadratowe/Obliczanie.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Pierwiastki
 {
-    public static void ObliczPierwiastki(double a, double b, double c)
+    public static (double x1, double x2) ObliczPierwiastki(double a, double b, double c)
     {       
         double delta = b * b - 4 * a * c;
 
@@ -11,15 +11,18 @@ public class Pierwiastki
             double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
             double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
             Console.WriteLine("Pierwiastki równania to: x1 = {0}, x2 = {1}", x1, x2);
+            return (x1, x2);
         }
         else if (delta == 0)
         {
             double x = -b / (2 * a);
             Console.WriteLine("Równanie ma jeden pierwiastek podwójny: x = {0}", x);
+            return (x, double.NaN);
         }
         else
         {
             Console.WriteLine("Równanie nie ma pierwiastków rzeczywistych.");
+            return (double.NaN, double.NaN);
         }
     }
 }
